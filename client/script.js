b533fa3067b59eaaee4ab7979edeeda190783e08
Anurag Kumar <ak7546824@gmail.com>
2026-05-11 02:09:10 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000";
+const API = "https://task-flow-79qo.onrender.com/";
 
 const form = document.getElementById("todoForm");
 const input = document.getElementById("taskInput");
